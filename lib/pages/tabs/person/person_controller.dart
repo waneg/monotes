a49173b084f8ec3f9ebc3a4e7648928547cd4350
hyummmm <799179889@qq.com>
2023-02-07 15:13:22 +0800

    person

--- lib/pages/tabs/person/person_controller.dart
+++ lib/pages/tabs/person/person_controller.dart
@@ -1,5 +1,15 @@
 import 'package:get/get.dart';
 
 class PersonController extends GetxController {
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
 
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
+  }
 }
